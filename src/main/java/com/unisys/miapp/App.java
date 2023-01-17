cc47e7de765bfe004057aa314f72a1bbddf73484
Moreno Martinez <Ruben.MorenoMartinez@unisys.com>
2023-01-17 12:20:06 +0100

    Initial commit

---
+++ src/main/java/com/unisys/miapp/App.java
@@ -0,0 +1,20 @@
+package com.unisys.miapp;
+
+public class App {
+    public static void main(String[] args) {
+        int entero=1;
+        short marianico=3;
+        long gasol=4;
+        boolean independencia=false;
+        double presupuesto=23.22;
+        float longitud=34;
+        System.out.println("entero= "+entero);
+        System.out.println("marianico= "+marianico);
+        System.out.println("gasol= "+gasol);
+        System.out.println("independencia= "+independencia);
+        System.out.println("prespuesto= "+presupuesto);
+        System.out.println("longitud= "+longitud);
+        System.out.println("suma= "+(gasol+marianico));
+
+    }
+}
---
+++ src/main/java/com/unisys/miapp2.java
@@ -0,0 +1,4 @@
+package com.unisys;
+
+public class miapp2 {
+}
